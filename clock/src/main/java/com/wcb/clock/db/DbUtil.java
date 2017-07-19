d6556294ab862eeb1bc4444dc3f59e2d358ae0af
RS <838544960@qq.com>
2017-07-19 18:16:02 +0800

    增加方法

--- clock/src/main/java/com/wcb/clock/db/DbUtil.java
+++ clock/src/main/java/com/wcb/clock/db/DbUtil.java
@@ -1,7 +1,13 @@
 package com.wcb.clock.db;
 
 import android.content.Context;
-import org.greenrobot.greendao.DaoException;
+import com.wcb.clock.db.obj.Clock;
+import com.wcb.clock.db.obj.ClockDao;
+import org.greenrobot.greendao.query.Query;
+import org.greenrobot.greendao.query.WhereCondition;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 数据库工具类
@@ -10,12 +16,43 @@ import org.greenrobot.greendao.DaoException;
 
 public class DbUtil {
 
-    //FIXME
-    public static void update(Context context, Object obj) {
-        try {
-//            DbManager.dbUpdate();
-        } catch (DaoException e) {
-
-        }
+    /**
+     * 获取数据库中所有闹钟
+     * @param context
+     * @return
+     */
+    public static List<Clock> getAllClock(Context context) {
+        Query<Clock> query = DbManager.dbQuery(context, Clock.class);
+        if (query != null)
+            return query.list();
+        return new ArrayList<>();
     }
+
+    /**
+     * 获取数据库中所有处于enable状态的闹钟
+     * @param context
+     * @return
+     */
+    public static List<Clock> getAllEnableClock(Context context) {
+        WhereCondition condition = ClockDao.Properties.Enable.eq(true);
+        Query<Clock> query = DbManager.dbQuery(context, Clock.class, condition);
+        if (query != null)
+            return query.list();
+        return new ArrayList<>();
+    }
+
+    /**
+     * 获取数据库中所有处于disable状态的闹钟
+     * @param context
+     * @return
+     */
+    public static List<Clock> getAllDisableClock(Context context) {
+        WhereCondition condition = ClockDao.Properties.Enable.eq(false);
+        Query<Clock> query = DbManager.dbQuery(context, Clock.class, condition);
+        if (query != null)
+            return query.list();
+        return new ArrayList<>();
+    }
+
+
 }
